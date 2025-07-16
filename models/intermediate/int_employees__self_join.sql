0ad1a8d1a079265228c4a758d772f045b73e8406
Eike Hirsch <eikehi@gmail.com>
2025-07-16 19:32:22 +0000

    Inserts null cleaning in manager_name and employee_region

--- models/intermediate/int_employees__self_join.sql
+++ models/intermediate/int_employees__self_join.sql
@@ -9,16 +9,36 @@ WITH
         select
             employees.employee_pk AS employee_pk
             , employees.employee_name AS employee_name
-            , managers.employee_name AS manager_name
+            , CASE
+                WHEN managers.employee_name IS NULL THEN 'N/A'
+                ELSE managers.employee_name
+              END AS manager_name
             , employees.employee_title AS employee_title
             , employees.employee_birth_date AS employee_birth_date
             , employees.employee_hire_date AS employee_hire_date
             , employees.employee_address AS employee_address
             , employees.employee_city AS employee_city
-            , employees.employee_region AS employee_region
+            , CASE
+                WHEN employees.employee_region IS NULL THEN 'N/A'
+                ELSE employees.employee_region
+              END AS employee_region
             , employees.employee_country AS employee_country
         from employees AS employees
         left join employees AS managers ON employees.manager_fk = managers.employee_pk
+    ),
+    cleaned AS (
+        select 
+        employee_pk
+        , employee_name
+        , manager_name
+        , employee_title
+        , employee_birth_date
+        , employee_hire_date
+        , employee_address
+        , employee_city
+        , employee_region
+        , employee_country
+        from joined
     )
 
 select * from joined
