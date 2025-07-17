62d9494dbef2026d7d83969b5f2c444c576a55df
Eike Hirsch <eikehi@gmail.com>
2025-07-17 14:34:54 +0000

    Creates dim_products enriched dimension

---
+++ models/intermediate/int_products__enriched.sql
@@ -0,0 +1,40 @@
+WITH 
+    --Importing model from staging
+    categories AS (
+        select * 
+        from {{ ref('stg_erp__categories') }}
+    )
+    --Importing model from intermediate
+    , suppliers AS (
+        select *
+        from {{ ref('int_suppliers__cleaned') }}
+    )
+    , products AS (
+        select *
+        from {{ ref('stg_erp__products') }}
+    )
+    -- Joining tables
+    , joined AS (
+        select 
+            products.product_pk
+            , products.product_name
+            , products.quantity_per_unit
+            , products.unit_price
+            , products.units_in_stock
+            , products.units_on_order
+            , products.is_discontinued
+            , suppliers.supplier_name
+            , suppliers.supplier_address
+            , suppliers.supplier_city
+            , suppliers.supplier_region
+            , suppliers.supplier_country
+            , suppliers.supplier_phone
+            , categories.category_name
+            , categories.category_description
+        from products
+        left join categories ON products.category_fk = categories.category_pk
+        left join suppliers ON products.supplier_fk = supplier_pk
+    )
+    
+    select * from joined
+   
